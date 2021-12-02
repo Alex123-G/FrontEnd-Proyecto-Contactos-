fc9d4caeba4b36f34b1bfc68a2c8891325f97aa0
Alex123-G <alexgallozo521@gmial.com>
2021-12-02 16:18:05 -0500

    change api direction

--- src/functions/function.js
+++ src/functions/function.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const obtenerTodosContactos = async state => {
-	const peticion = await axios.get("http://localhost:3001/obtener");
+	const peticion = await axios.get("https://backend-proyecto-contactos.herokuapp.com/obtener");
 	state(peticion.data);
 };
 
 export const insertaNuevoContacto = (nombre, numero) => {
-	axios.get(`http://localhost:3001/insertar/${nombre}/${numero}`);
+	axios.get(`https://backend-proyecto-contactos.herokuapp.com/insertar/${nombre}/${numero}`);
 };
 
 export const actualizarContacto = (id, nombre, numero) => {
-	axios.get(`http://localhost:3001/actualizar/${id}/${nombre}/${numero}`);
+	axios.get(`https://backend-proyecto-contactos.herokuapp.com/actualizar/${id}/${nombre}/${numero}`);
 };
 
 export const borrarContacto = id => {
-	axios.get(`http://localhost:3001/borrar/${id}`);
+	axios.get(`https://backend-proyecto-contactos.herokuapp.com/borrar/${id}`);
 };
